a0928f27a773ad20156b04a4c93081c673a224a5
aermolaev <aermolaev@svb.com>
2020-03-24 18:56:54 -0700

    Updates

---
+++ precache-manifest.2e3bb1f09b60f261800ba052b08bc109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b539ec841dc07bc8b595338e35a6336b",
+    "url": "/pdf-watcher-poc/index.html"
+  },
+  {
+    "revision": "57b7fadc48c02d78d769",
+    "url": "/pdf-watcher-poc/static/css/main.4be4efd0.chunk.css"
+  },
+  {
+    "revision": "2849d5f8c73939fdcc28",
+    "url": "/pdf-watcher-poc/static/js/2.4546b5d4.chunk.js"
+  },
+  {
+    "revision": "5142e35b1b9c983389a5a631d2c76ef5",
+    "url": "/pdf-watcher-poc/static/js/2.4546b5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b7fadc48c02d78d769",
+    "url": "/pdf-watcher-poc/static/js/main.b8c7c46e.chunk.js"
+  },
+  {
+    "revision": "570b2cadc8ef288b519d",
+    "url": "/pdf-watcher-poc/static/js/runtime-main.64c07321.js"
+  }
+]);
