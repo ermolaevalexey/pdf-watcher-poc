1b5a8db51cdf715d7569e3121b06ad3c78e96d56
aermolaev <aermolaev@svb.com>
2020-03-24 18:38:24 -0700

    fixes - moved local pdfs to public, added disclaimer, styles improvments

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 import { ReactPdfWatcher } from './ReactPdfWatcher';
 
 function App() {
+  const [url, setUrl] = useState('');
+
+  const updateUrl = useCallback((e: React.SyntheticEvent<HTMLInputElement>) => {
+    setUrl(e.currentTarget.value);
+  }, [url, setUrl]);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>PDF watcher POC</h1>
       </header>
       <div className="App-Content">
+        <h2>Enter a URL with PDF file of choice*</h2>
+        <h3>Examples:</h3>
+        <ul className="links">
+          <li><code>https://raw.githubusercontent.com/diegomura/react-pdf/master/examples/resume/output.pdf</code></li>
+          <li><code>https://raw.githubusercontent.com/diegomura/react-pdf/master/examples/pageWrap/output.pdf</code></li>
+          <li><code>https://raw.githubusercontent.com/diegomura/react-pdf/master/examples/knobs/output.pdf</code></li>
+          <li><code>/a-sample-pdf.pdf</code></li>
+          <li><code>/gre_research_validity_data.pdf</code></li>
+        </ul>
+        <div className="App-Form">
+          <input type="text" value={url} onChange={updateUrl} />
+          <span className="disclaimer">*External urls will work only with allowed cross-domain access</span>
+        </div>
         <div className="Watcher-container">
-          <ReactPdfWatcher
-            fileUrl={'https://raw.githubusercontent.com/diegomura/react-pdf/master/examples/resume/output.pdf'}
-          />
+          {url &&
+            <ReactPdfWatcher
+              fileUrl={url}
+            />
+          }
         </div>
       </div>
     </div>
