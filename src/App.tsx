a6218d9fa66cfe50df15946f24463662c9951f02
aermolaev <aermolaev@svb.com>
2020-03-24 18:01:32 -0700

    fixed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ReactPdfWatcher } from './ReactPdfWatcher';
 
@@ -7,20 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ReactPdfWatcher fileUrl={'https://raw.githubusercontent.com/diegomura/react-pdf/master/examples/resume/output.pdf'} />
+        <h1>PDF watcher POC</h1>
       </header>
+      <div className="App-Content">
+        <div className="Watcher-container">
+          <ReactPdfWatcher
+            fileUrl={'https://raw.githubusercontent.com/diegomura/react-pdf/master/examples/resume/output.pdf'}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/ReactPdfWatcher.tsx
+++ src/ReactPdfWatcher.tsx
@@ -1,41 +1,58 @@
-import * as React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { range } from 'lodash';
 import { Document, Page, pdfjs as pdfJs } from 'react-pdf';
-import { useCallback, useEffect, useState } from 'react';
+import './ReactPdfWatcher.css';
 
 pdfJs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfJs.version}/pdf.worker.js`;
+
 export interface ReactPdfWatcherProps {
-    fileUrl: string;
+  fileUrl: string;
 }
 
 function getFile(url: string) {
-    return fetch(url, {
-        headers: {
-            'Accept': 'application/pdf'
-        }
-    }).then(response => response.blob());
+  return fetch(url, {
+    headers: {
+      'Accept': 'application/pdf'
+    }
+  }).then(response => response.blob());
 }
 
 export function ReactPdfWatcher(props: ReactPdfWatcherProps) {
-    const [binaryFile, setBinaryFile] = useState<Blob | null>(null);
-    const [pages, setPages] = useState<pdfJs.PDFPageProxy[]>([]);
+  const [binaryFile, setBinaryFile] = useState<Blob | null>(null);
+  const [pagesCount, setPagesCount] = useState<number>(0);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-    const updateBinary = useCallback(async () => {
-        const file = await getFile(props.fileUrl);
-        setBinaryFile(file);
-    }, [binaryFile, setBinaryFile]);
+  const updateBinary = useCallback(async () => {
+    const file = await getFile(props.fileUrl);
+    setBinaryFile(file);
+  }, [props.fileUrl]);
 
-    useEffect(() => {
-        (async () => {
-            await updateBinary();
-        })();
-    }, []);
+  const updatePagesCount = useCallback(({ numPages }: pdfJs.PDFDocumentProxy) => {
+    setPagesCount(numPages);
+  }, [setPagesCount]);
 
-    return (binaryFile &&
-        <Document
-            file={binaryFile}
-        >
-            <Page pageNumber={1} width={600} />
-        </Document>
-    );
+  useEffect(() => {
+    (async () => {
+      await updateBinary();
+    })();
+  }, [updateBinary]);
+
+  return (binaryFile &&
+    <div ref={containerRef}>
+      <Document
+        className="document"
+        file={binaryFile}
+        onLoadSuccess={updatePagesCount}
+      >
+        {!!pagesCount && range(1, pagesCount + 1).map((pageNumber) =>
+          <Page
+            className="page"
+            key={pageNumber}
+            pageNumber={pageNumber}
+            width={(containerRef.current && containerRef.current!.clientWidth) || 0}
+          />
+        )}
+      </Document>
+    </div>
+  );
 }
